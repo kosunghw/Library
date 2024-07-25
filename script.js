49deadc85852f03c243ff0e08da0591f71626de6
Sung <sung272012@hotmail.com>
2024-07-25 02:32:37 -0400

    Add radio buttons for read

--- script.js
+++ script.js
@@ -92,49 +92,6 @@ function display() {
     bookShelf.appendChild(newCard);
     setElementId();
   }
-  //   for (const book of myLibrary) {
-  //     if (!displayLibrary.includes(book)) {
-  //       // IF SHELF IS EMPTY
-  //       // For every element in myLibrary,
-  //       // Create a new card div
-  //       const newCard = document.createElement("div");
-  //       // Create div.title, div.author, div.pages, div.read
-  //       // And append each to the card.
-  //       const title = document.createElement("div");
-  //       title.textContent = `${book.title}`;
-  //       title.classList.add("title");
-  //       const author = document.createElement("div");
-  //       author.textContent = `${book.author}`;
-  //       author.classList.add("author");
-  //       const pages = document.createElement("div");
-  //       pages.textContent = `${book.pages}p.`;
-  //       pages.classList.add("pages");
-  //       const read = document.createElement("div");
-  //       read.textContent = `${book.read}`;
-  //       read.classList.add("read");
-  //       const deleteBtn = document.createElement("button");
-  //       deleteBtn.textContent = "DELETE";
-  //       deleteBtn.classList.add("delete-button");
-  //       const readBtn = document.createElement("button");
-  //       readBtn.textContent = "READ";
-  //       readBtn.classList.add("read-button");
-
-  //       newCard.appendChild(title);
-  //       newCard.appendChild(author);
-  //       newCard.appendChild(pages);
-  //       newCard.appendChild(read);
-  //       newCard.appendChild(readBtn);
-  //       newCard.appendChild(deleteBtn);
-  //       newCard.classList.add("card");
-  //       newCard.classList.add("grid-container");
-
-  //       // Append the new card to the book shelf
-  //       bookShelf.appendChild(newCard);
-  //       displayLibrary.push(book);
-  //     }
-  //     setElementId();
-  //     continue;
-  //   }
 }
 
 const dialog = document.getElementById("new-book-dialog");
@@ -156,9 +113,16 @@ confirmButton.addEventListener("click", (e) => {
   const title = document.getElementById("form-title").value;
   const author = document.getElementById("form-author").value;
   const page = document.getElementById("form-page").value;
-  const read = document.getElementById("form-read").value;
-  const newBook = new Book(title, author, page, read);
-  addBookToLibrary(newBook);
+  const readYes = document.getElementById("read-yes");
+  if (readYes.checked) {
+    const read = "Yes";
+    const newBook = new Book(title, author, page, read);
+    addBookToLibrary(newBook);
+  } else {
+    const read = "Not read yet";
+    const newBook = new Book(title, author, page, read);
+    addBookToLibrary(newBook);
+  }
   display();
   e.preventDefault();
   form.reset();
@@ -189,13 +153,3 @@ function setElementId() {
     children[i].id = `${i}`;
   }
 }
-// const deleteBtn = document.querySelectorAll(".delete-button");
-// for (const btn of deleteBtn) {
-//   btn.addEventListener("click", () => {
-//     let index = btn.parentElement.indexNumber;
-//     let book = myLibrary[index];
-//     myLibrary.splice(index, 1);
-//     displayLibrary.splice(index, 1);
-//     btn.parentElement.remove();
-//   });
-// }
